3dfda32dd5cc2e212df6c0d4f8743cbc40e1950c
Iris Larin <iris.larin@riotinto.com>
2025-02-25 09:43:24 -0500

    Added vector Store, embeddings and retriever

---
+++ exercise-files/02/main.py
@@ -0,0 +1,52 @@
+from langchain_community.vectorstores import FAISS
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings, OPenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassThrough
+from dotenv import load_dotenv
+
+
+load_dotenv()
+model = OpenAI()
+
+template = """Answer the question based only on the following contexts
+ {Context}
+
+Question: {Question}
+
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+# create a vectorstore & embeddings : https://python.langchain.com/v0.2/docs/integrations/vectorstores/faiss/#add-items-to-vector-store
+# Creating embeddings: Converts text into a numerical format (vectors) that represents its meaning, making it easier for a machine to understand; 
+# Vector store: Stores these embeddings in a database, allowing efficient searching and retrieval based on semantic similarity.
+
+vectorstore = FAISS.from_texts(['harisson worked at kensho'], embedding=OpenAIEmbeddings())
+
+
+# querying the vectorstore --> Define a query; Run similarity search: Use the vector store's method to search for relevant documents based on the query;
+# Top_k parameter: Optionally limit the results to the top most relevant ones;Print results: Retrieve and display the content of the most relevant documents; The process returns document objects with content (and optional metadata) matching the query's embeddings.
+query = "where did harrisson work?"
+docs = vectorstore.similarity_search(query, top_k=1)
+print(docs[0].page_content)
+
+
+# querying vector store as retriever
+retriever = vectorstore.as_retriever()
+docs = retriever.invoke(query, top_k=1)
+print(docs[0].page_content)
+
+
+retrieval_chain = ( 
+        {
+            "contexts": retriever,
+            "question": RunnablePassThrough()
+        }
+        | prompt
+        | model
+        | StrOutputParser()
+)
+
+response = retrieval_chain.invoke(query)
+print(response)
